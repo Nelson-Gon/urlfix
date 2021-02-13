6ae25b8b58a7c581eddd66a0144cd9b00608c75f
Nelson-Gon <gonzabato@hotmail.com>
2021-02-13 21:41:25 +0800

    Improve file formatting

--- tests.py
+++ tests.py
@@ -2,16 +2,17 @@ import unittest
 from urlfix import urlfix
 import os
 
-
 dir_path = os.path.dirname(os.path.abspath(__file__))
 # Use the above to make paths to files, avoid changing directory just for tests.
 use_file = os.path.join(dir_path, "testurls.md")
+
+
 class Testurlfix(unittest.TestCase):
     def test_show_parsed_urls(self):
         # Check that the RegEx works as expected
         # TODO: Fix tests for non markdown mode
-        #res_txt = urlfix.show_parsed_urls("urls.txt")
-        #self.assertEqual(len(res_txt), 6)
+        # res_txt = urlfix.show_parsed_urls("urls.txt")
+        # self.assertEqual(len(res_txt), 6)
         # What happens if we have .md format
         res_md = urlfix.show_parsed_urls(use_file)
         self.assertEqual(len(res_md), 3)
@@ -27,5 +28,3 @@ class Testurlfix(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
-

1f11090892f9d5f36f891b4f624eb9a3348b947c
Sripada Pradhan <sripada.pradhan@protonmail.com>
2021-03-06 16:47:25 +0100

    Updated and added temp file feature

--- urlfix/urlfix.py
+++ urlfix/urlfix.py
@@ -19,10 +19,11 @@ class URLFix(object):
         matches = re.findall(format_pattern, self.input_file)
         self.input_format = matches[0] if len(matches) > 0 else ''
 
-    def replace_urls(self, verbose=False, correct_urls=None):
+    def replace_urls(self, verbose=False, correct_urls=None, inplace=False):
         """
         :param verbose Logical. Should you be notified of what URLs have moved? Defaults to False.
         :param correct_urls. A sequence of urls known to be correct.
+        :param inplace. Flag for inplace update operation.
         :return  Replaces outdated URL and writes to the specified file. It also returns the number of URLs that have
         changed. The latter is useful for tests.
         """
@@ -43,9 +44,14 @@ class URLFix(object):
         number_moved = 0
         number_of_urls = 0
 
-        if self.output_file is None:
+        if not self.output_file and not inplace:
             raise ValueError("Please provide an output file to write to.")
         output_file = self.output_file
+        if inplace:
+            output_file = "replacements_tmp.txt"
+            with open(output_file, "w"):
+                #Create temp file if temp file is not specified
+                pass
 
         if not all(os.path.isfile(x) for x in [self.input_file, output_file]):
             raise FileNotFoundError("input_file and output_file should be valid files.")
@@ -77,6 +83,13 @@ class URLFix(object):
                             print(f"{matched_url} replaced with {url_used} in {out_f.name}")
                     out_f.write(line.replace(matched_url, url_used))
 
+        if inplace:
+            os.remove(self.input_file)
+            if verbose:
+                print(f"{self.input_file} removed successfully")
+            os.rename(output_file, self.input_file)
+            if verbose:
+                print(f"{output_file} renamed with {self.input_file} successfully")
         information = "URLs have changed" if number_moved != 1 else "URL has changed"
         print(f"{number_moved} {information} of the {number_of_urls} links found in {self.input_file}")
         return number_moved
@@ -93,22 +106,25 @@ class DirURLFix(object):
     def __replace_by_format(self, _format, **kwargs):
         """
         :param _format: Input format of the files. Currently supports md and txt
+        :param inplace: Flag for inplace update operation.
         :return: File with outdated URLs replaced.
         """
-
         for input_file in self.use_files.glob(f'*.{_format}'):
-            input_file = str(input_file)  # convert object to path string
-            if '_output' in input_file:
-                print(f"File is a fix of another file: {input_file}")
-                continue  # skip output files
+            input_file = str(input_file) # convert object to path string
+            if "inplace" in kwargs and kwargs["inplace"]:
+                output_file = "replacements_tmp.txt"
+            else:
+                if '_output' in input_file:
+                    print(f"File is a fix of another file: {input_file}")
+                    continue  # skip output files
 
-            output_file = input_file.replace(f'.{_format}', f'_output.{_format}')
-            if os.path.exists(output_file):
-                print(f"File already fixed: {input_file}")
-                continue  # skip file that's already been fixed
+                output_file = input_file.replace(f'.{_format}', f'_output.{_format}')
+                if os.path.exists(output_file):
+                    print(f"File already fixed: {input_file}")
+                    continue  # skip file that's already been fixed
 
-            with open(output_file, 'w'):
-                pass  # create an empty output file
+                with open(output_file, 'w'):
+                    pass  # create an empty output file
             return URLFix(input_file, output_file).replace_urls(**kwargs)
 
     def replace_urls(self, **kwargs):
